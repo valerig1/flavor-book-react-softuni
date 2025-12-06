80221f7bafc048f2ba1a86e8921dee14705614c0
valerig1 <valerig742@gmail.com>
2025-12-06 16:44:28 +0200

    Update app component
    Add context provider and logout route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,27 +5,28 @@ import Home from "./components/home/Home"
 import Login from "./components/login/Login"
 import Register from "./components/register/Register"
 import RecipeCreate from "./components/recipe-create/RecipeCreate"
+import Logout from "./components/logout/Logout"
+import { UserProvider } from "./contexts/UserContext"
 
 function App() {
-
   return (
-    <div className="min-h-screen flex flex-col">
+    <UserProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
 
-      <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/recipes/create" element={<RecipeCreate />} />
+          </Routes>
+        </main>
 
-      <main className="flex-1">
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/recipes/create" element={<RecipeCreate />} />
-        </Routes>
-      </main>
-
-      <Footer />
-
-    </div>
+        <Footer />
+      </div>
+    </UserProvider>
   )
 }
 
