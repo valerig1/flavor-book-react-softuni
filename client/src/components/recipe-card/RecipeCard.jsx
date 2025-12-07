bafefdf6973e7e8ba30e8ed42bd4a97550d320ca
valerig1 <valerig742@gmail.com>
2025-12-07 15:52:51 +0200

    Update RecipeCard with dynamic data

--- client/src/components/recipe-card/RecipeCard.jsx
+++ client/src/components/recipe-card/RecipeCard.jsx
@@ -1,5 +1,6 @@
 export default function RecipeCard({
     name,
+    description,
     img,
 
 }) {
@@ -15,9 +16,7 @@ export default function RecipeCard({
                 <h2 className="text-xl font-semibold mb-2">{name}</h2>
 
                 <div className="flex items-center justify-between">
-                    <p className="text-gray-600 text-sm truncate max-w-[80%]">
-                        A classic Italian pasta dish with a rich meat sauce.
-                    </p>
+                    <p className="text-gray-600 text-sm truncate max-w-[80%]">{description}</p>
 
                     <button className="bg-blue-600 hover:bg-blue-700 text-white text-sm px-5 py-1 rounded-md transition ml-4 shrink-0">
                         View Recipe
