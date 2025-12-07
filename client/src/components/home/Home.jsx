181bb585ec50505a57ba734ebe7fad49b60957b6
valerig1 <valerig742@gmail.com>
2025-12-07 15:51:55 +0200

    Refactor home page to fetch latest 3 recipes

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,31 +1,22 @@
-import { useEffect, useState } from "react";
 import RecipeCard from "../recipe-card/RecipeCard";
+import useRequest from "../../hooks/useRequest";
 
 export default function Home() {
-    const [allRecipes, setAllRecipes] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3030/data/recipes')
-            .then(response => response.json())
-            .then(result => setAllRecipes(result))
-            .catch(err => {
-                alert(err.message);
-            })
-    }, []);
+    const { data: latestRecipes } = useRequest('/data/recipes?sortBy=_createdOn%20desc&pageSize=3', []);
 
     return (
         <div className="p-8">
             <h1 className="text-3xl font-bold mb-6">Welcome to FlavorBook</h1>
             <p className="text-gray-700 mb-8">
-                Discover delicious recipes and start cooking today!
+                Discover our latest delicious recipes and start cooking today!
             </p>
 
             {/* Recipe Cards */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 
-                {allRecipes.length > 0
+                {latestRecipes.length > 0
                     ?
-                    (allRecipes.map(recipe => <RecipeCard key={recipe._id} {...recipe} />))
+                    (latestRecipes.map(recipe => <RecipeCard key={recipe._id} {...recipe} />))
                     : <p className="col-span-full text-center text-gray-500 text-lg py-10">
                         No recipes yet
                     </p>
